8ceef3e3089c3531067a80774eb6aa3410bd9880
Sachinpathak098 <124699279+Sachinpathak098@users.noreply.github.com>
2026-03-23 12:24:21 +0530

    AccountMenu.js

--- components/AccountMenu.js
+++ components/AccountMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { Box, Avatar, Menu, MenuItem, ListItemIcon, Divider, IconButton, Tooltip } from '@mui/material';
 import { Settings, Logout } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
